24b301bfb93c5e669ead1949c06f97c9dac33bad
Michael Efemena <mefemena@gmail.com>
2023-10-29 16:13:15 +0100

    add encodeUrl and decodeUrl functions to urlShortner controller

---
+++ urlShortner/urlShortner.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from "express";
+import { UrlShortnerService } from "./urlShortner.service";
+
+export default class UrlShortnerController {
+  constructor(private urlShortnerService: UrlShortnerService) {}
+
+  public async encodeUrl(req: Request, res: Response) {
+    try {
+      const { url } = req.body;
+      const result = await this.urlShortnerService.encodeUrl(url);
+
+      return {
+        message: "Url encoded successfully",
+        data: result,
+      };
+    } catch (error) {
+      console.log(error);
+      throw new Error("Error while encoding url");
+    }
+  }
+
+  public async decodeUrl(req: Request, res: Response) {
+    try {
+      const { shortUrl } = req.params;
+      const result = await this.urlShortnerService.decodeUrl(shortUrl);
+
+      return {
+        message: "Url decoded successfully",
+        data: result,
+      };
+    } catch (error) {
+      console.log(error);
+      throw new Error("Error while decoding url");
+    }
+  }
+}
