59f30b6b948e11afe3f938ac51ae4a6c1c7832be
Michael Efemena <mefemena@gmail.com>
2023-10-29 11:59:53 +0100

    add build script to package.json

--- server.ts
+++ server.ts
@@ -1,8 +1,9 @@
 import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app: Express = express();
-
-
 
 app.listen(3003, () => {
   console.log("Server running on port 3003");
