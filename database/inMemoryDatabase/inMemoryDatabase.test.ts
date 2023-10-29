693c7ec3ca8ae1c4ff1c8d198e8a22aa9198395f
Michael Efemena <mefemena@gmail.com>
2023-10-29 15:44:12 +0100

    add set,get,delete and update tests for in memory database

--- database/inMemoryDatabase/inMemoryDatabase.test.ts
+++ database/inMemoryDatabase/inMemoryDatabase.test.ts
@@ -20,4 +20,23 @@ describe("InMemoryDatabaseTest", () => {
     expect(setSpy).toHaveBeenCalled();
     expect(value).toEqual({ test: "test" });
   });
+
+  test("should get a value from the database", async () => {
+    await db.set("test", { test: "test" });
+    const value = await db.get("test");
+    expect(value).toEqual({ test: "test" });
+  });
+
+  test("should delete a value from the database", async () => {
+    await db.set("test", { test: "test" });
+    await db.delete("test");
+    const value = await db.get("test");
+    expect(value).toBeUndefined();
+  });
+
+  test("should update a value in the database", async () => {
+    await db.set("test", { test: "test" });
+    const value = await db.update("test", { test: "updated", test2: "test2" });
+    expect(value).toEqual({ test: "updated", test2: "test2" });
+  });
 });
