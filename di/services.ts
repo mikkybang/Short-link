d1cb5bb21c6e323707bd85ac566c5d8f0db80ecb
Michael Efemena <mefemena@gmail.com>
2023-10-30 06:00:49 +0100

    remove rsdi package and all files

--- di/services.ts
+++
@@ -1,6 +0,0 @@
-import db from "../database/inMemoryDatabase";
-import { UrlShortnerService } from "../urlShortner/urlShortner.service";
-
-export const services = {
-  [UrlShortnerService.name]: new UrlShortnerService(db),
-};
--- server.ts
+++ server.ts
@@ -1,12 +1,16 @@
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import db from "./database/inMemoryDatabase";
+import configureDI from "./di";
 
 dotenv.config();
 
 const app: Express = express();
 
 const port = process.env.PORT || 3003;
+
+const diContainer = configureDI();
+
 
 db.connect();
 
