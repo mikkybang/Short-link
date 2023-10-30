46ddc18ffcb6d2355611c10a50cac903bf02f5a1
Michael Efemena <mefemena@gmail.com>
2023-10-30 11:17:30 +0100

    format url being saved

--- urlShortner/urlShortner.service.ts
+++ urlShortner/urlShortner.service.ts
@@ -23,14 +23,18 @@ export class UrlShortnerService {
       throw new Error("Invalid url");
     }
 
+    const uniqueValue = `${url}-${this.generateRandomId()}}`;
+
+    console.log(uniqueValue);
+
     const urlHash = crypto
       .createHash("sha256")
-      .update(url)
+      .update(uniqueValue)
       .digest("hex")
       .substring(0, 6);
     const shortURLData: UrlEntity = {
       hash: urlHash,
-      originalUrl: url,
+      originalUrl: this.formatUrl(url),
       sources: {},
       createdAt: new Date(),
       lastHitAt: null,
@@ -48,5 +52,22 @@ export class UrlShortnerService {
     const urlRegex =
       /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([\/\w .-]*)*\/?$/;
     return urlRegex.test(url);
+  }
+
+  generateRandomId() {
+    const timestamp = Date.now().toString(36);
+    const randomString = Math.random().toString(36).substring(2, 8);
+    return timestamp + randomString;
+  }
+
+  formatUrl(url: string) {
+    if (!/^https?:\/\//i.test(url)) {
+      url = "http://" + url;
+    }
+    if (url.endsWith("/")) {
+      url = url.slice(0, -1);
+    }
+    url = url.toLowerCase();
+    return url;
   }
 }
