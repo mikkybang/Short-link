132d1337d4f79ac48a2a7e11d2bdc065fdc04b78
Michael Efemena <mefemena@gmail.com>
2023-10-30 05:35:33 +0100

    add configDI function

--- di/index.ts
+++ di/index.ts
@@ -1,1 +1,11 @@
+import DIContainer, { object } from "rsdi";
 
+import { services } from "./services";
+
+export default function configureDI() {
+  const container = new DIContainer();
+
+  return container;
+}
+
+configureDI();
